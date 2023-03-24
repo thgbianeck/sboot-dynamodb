e6e346fea8866e46f9ed4fc5872ae5d57b83dbb9
Bianeck <thiagobianeck@gmail.com>
2023-03-24 19:42:28 -0300

    Initial commit

---
+++ src/main/java/br/com/bieniek/sbootdynamodb/SbootDynamodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.sbootdynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootDynamodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootDynamodbApplication.class, args);
+	}
+
+}
